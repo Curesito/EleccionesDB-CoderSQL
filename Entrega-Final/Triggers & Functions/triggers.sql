d14d3556201375232bb7bcdb39193647b30cfaa0
Curesito <142647487+Curesito@users.noreply.github.com>
2025-04-14 15:12:28 -0300

    Add files via upload

---
+++ Entrega-Final/Triggers & Functions/triggers.sql
@@ -0,0 +1,36 @@
+
+DELIMITER //
+
+CREATE TRIGGER EvitarVotoDoble
+BEFORE INSERT ON historialVoto
+FOR EACH ROW
+BEGIN
+    IF EXISTS (
+        SELECT 1 FROM historialVoto
+        WHERE votanteID = NEW.votanteID
+          AND eleccionID = NEW.eleccionID
+    ) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Fraude detectado: este votante ya votó en esta elección.';
+    END IF;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE TRIGGER ActualizarTotalVotos
+AFTER INSERT ON Voto
+FOR EACH ROW
+BEGIN
+    -- Actualiza el número de votos totales para una elección específica
+    UPDATE Eleccion
+    SET totalVotos = (
+        SELECT COUNT(*) FROM Voto WHERE eleccionID = NEW.eleccionID
+    )
+    WHERE eleccionID = NEW.eleccionID;
+END //
+
+DELIMITER ;
+
+CALL RegistrarVoto(1, 2, NOW());
